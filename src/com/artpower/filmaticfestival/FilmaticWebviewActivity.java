09e89305ca7171ef2037cf3998732da050480d1b
Kzip <jones@192.168.2.12>
2014-02-01 21:04:55 -0800

    Trying to figgure out how to swtich between horizontal and virtical

--- src/com/artpower/filmaticfestival/FilmaticWebviewActivity.java
+++ src/com/artpower/filmaticfestival/FilmaticWebviewActivity.java
@@ -1,5 +1,22 @@
 package com.artpower.filmaticfestival;
 
-public class FilmaticWebviewActivity {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FilmaticWebviewActivity extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
+	
+	
+	
+	
 
 }
